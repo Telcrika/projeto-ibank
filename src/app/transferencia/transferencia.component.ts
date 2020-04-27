a3542adbd864f1b8156763f8589c1312da05c364
Telma Cristina Gabriel <telcrigafenias@hotmail.com>
2020-04-27 20:08:01 -0300

    Testamos funcionalidades dos inputs

--- src/app/transferencia/transferencia.component.ts
+++ src/app/transferencia/transferencia.component.ts
@@ -7,9 +7,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TransferenciaComponent implements OnInit {
 
+  numeroConta = '';
+  numeroTransferencia = '';
+  transferirValor = '';
+  saldo = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  confirmarDados(){
+    console.log ('Babú perdeu o Big Brother');
+    /*this.http.post("http://localhost:57434/api/clientes/login", {
+      Conta: this.numeroConta,
+      transferencia: this.numeroTransferencia,
+      valor: this.transferirValor
+    }).subscribe((dados: any)=>{
+      this.cliente = dados;
+      console.log(this.cliente)
+    });*/
+     
+    
+
+  }
+
+  valorInput = document.getElementById("valorInput");
+  
+  
+
+  digitouNumero($event){
+    this.numeroConta = $event.target.value;
+    console.log($event.target.value);
+  }
+
+  digitouTransferencia($event){
+    this.numeroTransferencia = $event.target.value;
+    console.log($event.target.value);
+  }
+
+  digitouValor($event){
+    this.transferirValor = $event.target.value;
+    console.log($event.target.value);
+  }
+
+  transacaoUsuario() {
+    var saldo = 500;
+    
+    var transferirValor = 300;
+    var novoSaldo = saldo - transferirValor;
+    console.log("seu novo saldo e: ",novoSaldo);
+    alert ("A sua transferência foi feita com sucesso!")
+
+   
+ }
+
+  
+
+
+
+  
 
 }
+
+
+
